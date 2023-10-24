55b6135f9d346338fba935888cc88548b0dac728
Manish <manish.na@kpn.com>
2023-10-24 11:58:09 +0530

    File Crop by celery woker Ready

--- files/views.py
+++ files/views.py
@@ -2,12 +2,24 @@ from django.shortcuts import render
 from rest_framework.generics import CreateAPIView, ListAPIView
 from .models import File
 from .serializers import FileSerializer
-
+from tasks.tasks import crop_file
+from django.conf import settings
 # Create your views here.
 class UploadFiles(CreateAPIView):
     
     queryset = File.objects.all()
     serializer_class = FileSerializer
+    
+    def post(self, request, *args, **kwargs):
+
+        file = request.data.get("file")
+        
+        response = super().post(request, *args, **kwargs)
+        
+        file_path = f"{settings.MEDIA_ROOT}/files/{file.name}".strip()
+        print(file_path)
+        crop_file.delay(file_path,file.name)
+        return response
     
     
 class FilesList(ListAPIView):
--- tasks/tasks.py
+++ tasks/tasks.py
@@ -1,5 +1,22 @@
 from celery import shared_task
+from PIL import Image
+import os
+from django.conf import settings
+
+FOLDER = "cropped_files"
+OUTPUT_FOLDER_PATH = os.path.join(settings.BASE_DIR,FOLDER)
 
 @shared_task
-def task1():
-    return
+def crop_file(file_path,filename):
+    try:
+        img = Image.open(file_path)
+        print(img)
+        print(filename)
+        img.thumbnail((200,200))
+        if not os.path.exists(OUTPUT_FOLDER_PATH):
+            os.makedirs(OUTPUT_FOLDER_PATH)
+        img.save(os.path.join(OUTPUT_FOLDER_PATH,"thumbnail_"+filename))
+    except FileNotFoundError:
+        print(f"File Not Found with Path : ",file_path)
+    except Exception as ex:
+        print(f"Exception raised : ",ex)
